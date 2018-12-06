372f9b679fb8e2ccddbee28aa2278635d89a7fce
Ryan Boehm <ryan.w.boehm@gmail.com>
2018-12-06 01:50:37 -0500

    updated gapminder viz

--- project_2/js/main.js
+++ project_2/js/main.js
@@ -4,12 +4,14 @@
 *    Project 2 - Gapminder Clone
 */
 
-var margin = { left:100, right:10, top:10, bottom: 150 };
+var margin = { left:100, right:100, top:100, bottom: 200 };
 
-var width = 600 - margin.left - margin.right;
-var height = 400 - margin.top - margin.bottom;
+var width = 900 - margin.left - margin.right;
+var height = 600 - margin.top - margin.bottom;
 
 var flag = true;
+
+var year = 0;
 
 var t = d3.transition().duration(750);
 
@@ -30,17 +32,25 @@ var yAxisGroup = g.append("g")
   .attr("class", "y-axis");
 
 
-// X Scale
+// X Scale - income per capita
 var x = d3.scaleLog()
 	.domain([300, 150000])
-  .range([0,width])
+  .range([0, width])
   .base(10);
 
 
-// Y Scale
+// Y Scale - life expectancy
 var y = d3.scaleLinear()
 	.domain([0, 90])
-  .range([height,0]);
+  .range([height, 0]);
+
+
+// Color Scale for continents
+var color = d3.scaleOrdinal()
+  .domain(["africa","n. america","europe",
+          "s. america", "asia", "australasia"])
+  .range(["red", "orange", "yellow", "green",
+          "blue", "indigo", "grey"]);
 
 
 // X Label
@@ -52,6 +62,7 @@ g.append("text")
   .attr("text-anchor", "middle")
   .text("GDP Per Capita ($)");
 
+
 // Y Label
 var yLabel = g.append("text")
   .attr("class", "y axis-label")
@@ -62,55 +73,77 @@ var yLabel = g.append("text")
   .attr("transform", "rotate(-90)")
   .text("Life Expectancy (Years)");
 
-d3.json("data/data.json").then(function(data){
-	console.log(data);
 
+// Bind data
+d3.json("data/data.json").then(function(data){
+  dataset = data[year]['countries'];
+
+
+  // filter out null values from the array of countries for the selected year.
+  dataset = dataset.filter(function (el) {
+    return el.country != null && el.income != null && el.population != null;
+  });
+
+
+	console.log(dataset);
+
+
+  // X Axis
+  var xAxisCall = d3.axisBottom(x)
+    .tickValues([400, 4000, 40000])
+    .tickFormat( function(d) { return "$" + d; });
+  xAxisGroup.transition(t).call(xAxisCall);
+
+
+  // Y Axis
+  var yAxisCall = d3.axisLeft(y)
+    .tickFormat( function(d) { return d; });
+  yAxisGroup.transition(t).call(yAxisCall);
+
+  // Size of circle
+  var size = d3.scaleLinear()
+    .domain([0, d3.max(dataset, function(d) { return d.population; })])
+    .range([5, 25]);
+
+
+  // bind data to circles for the selected year.
+  g.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+      .attr("fill", function(d) { return color(d.continent)})
+      .attr("cy", function(d) { return y(d.life_exp) })
+      .attr("cx", function(d) { return x(d.income / d.population) })
+      .attr("r", function(d) { return (Math.PI * Math.pow(size(d.population), 2)) });
+
+  /*
   d3.interval( function() {
     update(data);
+    year += 1;
   }, 100);
+  */
 
   // run the vis for the first time
-  update(data);
+  //update(data);
 });
 
 
 function update(data) {
+  // remove any country-year combos that have a null value for life expectancy, income, or population
+
+
   // X Axis
-  var xAxisCall = d3.axisBottom(x);
-  xAxisGroup.transition(t).call(xAxisCall);
+
 
   // Y Axis
-  var yAxisCall = d3.axisLeft(y)
-    .tickFormat( function(d) { return "$" + d; });
-  yAxisGroup.transition(t).call(yAxisCall);
+
 
   // JOIN new data with old elements
-  var circles = g.selectAll("circle")
-    .data(data, function(d) {
-      return d.month;
-    });
+
 
   // EXIT old elements not present in new data.
-  circles.exit()
-    .attr("fill", "red")
-  .transition(t)
-    .attr("cy", y(0))
-    .remove();
 
 
   // ENTER new elements present in new data.
-  circles.enter()
-    .append("circle")
-      .attr("fill", "grey")
-      .attr("cy", y(0))
-      .attr("cx", function(d) { return x(d.month) + x.bandwidth() / 2 })
-      .attr("r", 5)
-      // AND UPDATE old elements present in new data.
-      .merge(circles)
-      .transition(t)
-        .attr("cx", function(d) { return x(d.month)  + x.bandwidth() / 2 })
-        .attr("cy", function(d) { return y(d[value]); })
 
-  var label = flag ? "Revenue" : "Profit";
-  yLabel.text(label);
 }
